388f7ec4277d560970ba9cbb6ad079265d58dbf9
Nikolay37rus <76645310+Nikolay37rus@users.noreply.github.com>
2021-10-01 16:16:43 +0300

    first commit

---
+++ ДЗ к уроку 7 основы сишарп в Юнити/ДЗ к уроку 7 основы сишарп в Юнити/Program.cs
@@ -0,0 +1,33 @@
+﻿//2. Реализовать метод расширения для поиска количество символов в строке.
+using System;
+using System.Text;
+
+public sealed class App
+{
+    static void Main()
+    {
+        
+        StringBuilder sb = new StringBuilder("ABC", 50);
+
+        
+        sb.Append(new char[] { 'D', 'E', 'F' });
+
+        
+        sb.AppendFormat("GHI{0}{1}", 'J', 'k');
+
+        
+        Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+
+        
+        sb.Insert(0, "Alphabet: ");
+
+        
+        sb.Replace('k', 'K');
+
+        
+        Console.WriteLine("{0} chars: {1}", sb.Length, sb.ToString());
+
+        Console.ReadKey();
+    }
+}
+
---
+++ ДЗ к уроку 7 основы сишарп в Юнити/Задание 3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _Задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            var data = GenerationData(15000000);
+            timer.Stop();
+            timer.Start();
+            data.Cast<string>()
+                .GroupBy(x => x)
+                .OrderBy(x => x.LongCount())
+                .ToList()
+                .ForEach(x => Console.WriteLine("Key: {0}, Count: {1}", x.Key, x.LongCount()));
+            timer.Stop();
+            Console.ReadLine();
+        }
+
+        public static StringCollection GenerationData(int count)
+        {
+            var random = new Random();
+            var result = new StringCollection();
+            for (var i = 0; i < count; i++)
+                result.Add(Convert.ToString(random.Next(0, 1000)));
+            return result;
+        }
+    }
+}
